8653c4f54b8f3a1803eea61e970c7ff6ad32c17c
Issarakhin <83199507+Issarakhin@users.noreply.github.com>
2026-03-16 13:20:33 +0700

    Update firebase-admin.ts

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,16 +1,13 @@
 import { initializeApp, getApps, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
-import { getStorage } from 'firebase-admin/storage';
 
 function initAdmin() {
   if (getApps().length === 0) {
-    const serviceAccount = JSON.parse(
-      process.env.FIREBASE_SERVICE_ACCOUNT_KEY || '{}'
-    );
+    const raw = process.env.FIREBASE_SERVICE_ACCOUNT_KEY || '{}';
+    const serviceAccount = JSON.parse(raw.replace(/\\n/g, '\n'));
     initializeApp({
       credential: cert(serviceAccount),
-      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     });
   }
 }
@@ -19,4 +16,3 @@ initAdmin();
 
 export const adminDb = getFirestore();
 export const adminAuth = getAuth();
-export const adminStorage = getStorage();
